161c61b7eed39d7658d6e0f0fe3cd641ba6be778
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2023-05-09 19:34:24 +0100

    Create login.js

---
+++ public/login.js
@@ -0,0 +1,17 @@
+function login(e){
+    e.preventDefault()
+    fetch("/panel/login", {
+        method: "POST",
+        body: JSON.stringify({
+            password: e.target[0].value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(data => data.text()).then(data => {
+        if(data === "ok") window.location = "/panel"
+        else alert(data)
+    })
+}
+
+document.getElementById("login-form").addEventListener("submit", login)
